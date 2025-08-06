e46a21d57a166e2d3a56b48accdd011ebdba1fb5
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-08-06 16:35:59 +0000

    "feat: add sign up"

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from './schema'
+import "dotenv/config";
 
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
 export const db = drizzle(process.env.DATABASE_URL!, {
-    schema,
+  schema,
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,36 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db";
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
-     emailAndPassword: { 
-     enabled: true, 
-  }, 
-     database: drizzleAdapter(db, {
-     provider: "pg",
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
   }),
+  user: {
+    modelName: "userTable",
+  },
+  session: {
+    modelName: "sessionTable",
+  },
+  account: {
+    modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
+  },
+   trustedOrigins: [
+    "https://glowing-trout-grrv5p4xpwwhg4x-3000.app.github.dev",
+    "https://localhost:3000",
+  ],
 });
