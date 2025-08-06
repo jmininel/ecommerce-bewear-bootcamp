59d740285dcb5e4bc4dbf474b14536033134bb3e
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-08-06 17:30:26 +0000

    "feat: add sign out"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Header } from "@/components/common/header";
+
+
 
 export default function Home() {
   return (
-   <div >
-    <Button>Bootcamp</Button>
-   </div>
-  );
+   <Header/>
+ );
 }
