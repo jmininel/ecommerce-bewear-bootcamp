b6923208d37df992df787a8a0c8a7efbaf2d916f
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-08-07 16:14:34 +0000

    "chore: fix image render"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fsc-projects-static.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
