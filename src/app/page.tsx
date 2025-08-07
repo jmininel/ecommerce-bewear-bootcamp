6e9e3f4c40d3b654b38323cfaebd4f8964ded0fb
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-08-07 16:15:52 +0000

    "feat: add product list and product item component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+import Image from "next/image";
+
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import CategorySelector from "@/components/common/category-selector";
 
-
-
-export default function Home() {
+const Home = async () => {
+   const products = await db.query.productTable.findMany({
+      with: {
+         variants: true,
+      }
+   })
+  
+  const categories = await db.query.categoryTable.findMany({})
+   
   return (
-   <Header/>
+  <>
+     <Header/>
+      <div className="space-y-6">
+     <div className="px-5">
+       <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw" 
+          className="h-auto w-full" 
+       />
+     </div>
+
+         <ProductList products={products} title="Mais vendidos"/>
+
+          <div>
+            <CategorySelector categories={categories}/>
+          </div>
+
+       <div className="px-5">
+          <Image
+          src="/banner-02.png"
+          alt="Leve uma vida com estilo"
+          height={0}
+          width={0}
+          sizes="100vw" 
+          className="h-auto w-full" 
+       />
+       </div>
+     </div>
+  </>
  );
 }
+
+export default Home
